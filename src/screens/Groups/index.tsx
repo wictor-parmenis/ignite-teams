fabc714e989b4246c66f8f7b06667d70d6f7b3ee
wictor-parmenis <wictortec@gmail.com>
2022-12-20 07:36:04 -0300

    feat: init project

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Groups: React.FC = () => {
+  return <View><Text>test</Text></View>
+}
+
+export {Groups};
